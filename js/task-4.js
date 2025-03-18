ab522830989a9369dce40aaed1c8fe8e02b8e26d
OlhaYakym <olhayakym@gmail.com>
2025-03-18 17:05:08 +0100

    HW_7_v.2

--- js/task-4.js
+++ js/task-4.js
@@ -1,23 +1,24 @@
-const formLogin = document.querySelector('.login-form');
+const formLogin = document.querySelector(".login-form");
 
 const sendUserInfo = (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const login = form.elements.email.value;
-    const password = form.elements.password.value;
+  event.preventDefault();
+  const form = event.target;
+  const login = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
 
-    if (!login || !password) {
-        alert('All form fields must be filled in');
-        return;
-    }
+  if (!login || !password) {
+    alert("All form fields must be filled in");
+    return;
+  }
 
-    const userInfo = {};
-    userInfo[form.elements.email.name] = login.trim();
-    userInfo[form.elements.password.name] = password.trim();
+  const userInfo = {
+    [form.elements.email.name]: login,
+    [form.elements.password.name]: password,
+  };
 
-    console.log(userInfo);
+  console.log(userInfo);
 
-    formLogin.reset();
-}
+  formLogin.reset();
+};
 
-formLogin.addEventListener('submit', sendUserInfo);
+formLogin.addEventListener("submit", sendUserInfo);
